bbf6974f11be7fa2b03f030e9f35fff0b87d4a71
Jevan-G <97144346+Jevan-G@users.noreply.github.com>
2023-04-09 12:11:00 -0700

    Adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -6,17 +6,21 @@ let places = [{
     state: 'WA',
     cuisines: 'Thai, Pan-Asian',
     pic: '/images/best-thai-restaurant.jpg'
-  }, {
-      name: 'Coding Cat Cafe',
-      city: 'Phoenix',
-      state: 'AZ',
-      cuisines: 'Coffee, Bakery',
-      pic: '/images/A-Cat-Cafe.jpg'
-  }]  
-  
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/A-Cat-Cafe.jpg'
+}]
+
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
 
 router.get('/', (req, res) => {
     res.render('places/index', { data: places });
-  });
+});
 
 module.exports = router
